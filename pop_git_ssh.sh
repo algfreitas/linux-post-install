145d672dcad4f5be9ab8de1afacabcf99be28e06
algfreitas <andrelgfreitas@gmail.com>
2021-12-15 23:24:25 -0300

    Initial commit

---
+++ pop_git_ssh.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+# Description: Script for enviroment configuration for Pop!_OS 21.04
+# Author: Andre L G Freitas
+# Date: 2021/01/20
+
+# Verify for sudo/root execution
+if [ "$EUID" -ne 0 ]
+  then echo "Por favor, execute o script como sudo!"
+  exit
+fi
+
+# Get the Real Username
+RUID=$(who | awk 'FNR == 1 {print $1}')
+
+# Translate Real Username to Real User ID
+RUSER_UID=$(id -u ${RUID})
+
+# Generate SSH Key for git
+sudo -u $SUDO_USER ssh-keygen -q -t rsa -N '' -f /home/$SUDO_USER/.ssh/id_rsa
+clear
+echo "Abra https://bitbucket.org/account/settings/ssh-keys/ no seu browser e faça a adição da chave acima."
+echo ""
+cat /home/$SUDO_USER/.ssh/id_rsa.pub
+echo ""
+read -p "Quando estiver pronto, pressione qualquer tecla para continuar... " temp </dev/tty
+
+# Set global git configuration
+clear
+echo "Agora vamos configurar suas credenciais locais do git."
+echo ""
+echo "Nome e sobrenome: "  
+read nome </dev/tty
+echo "E-mail: "  
+read email </dev/tty
+sudo -u $SUDO_USER git config --global user.name "$nome"
+sudo -u $SUDO_USER git config --global user.email "$email"
+clear
+
+# Alert 
+clear
+read -p "Configurado, pressione qualquer tecla para continuar..." temp </dev/tty
+
+# Bye :)
+
+
